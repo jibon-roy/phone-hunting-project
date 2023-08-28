0e651dc2edd84e4057fe262e328a8d8f49e18216
Jibon Krishna Roy <mailme.jibon@gmail.com>
2023-08-28 10:39:31 +0600

    Fatch called in function

--- scripts/fatch.js
+++ scripts/fatch.js
@@ -1,1 +1,7 @@
-fetch('https://')
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    console.log(data);
+}
+
+loadPhone();
